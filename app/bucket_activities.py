363122a22e46c4014403036cfeea2279e0c73695
Winston <winstonkamau77@gmail.com>
2017-07-20 07:42:41 +0300

    added test an crud

--- app/bucket_activities.py
+++ app/bucket_activities.py
@@ -1,4 +1,4 @@
-'''A module that contains a class that stores a bucket and a n activity list'''
+'''A module that contains a class that stores a bucket (key) and an activity list(value)'''
 bucket_name = ""
 activity_list = []
 list_items = { bucket_name : activity_list}
@@ -9,33 +9,31 @@ class BucketActivities(object):
         self.activity_list = activity_list
     def create_bucket(self, item_added):
         if item_added !=  "" and isinstance (item_added , str):
-            self.list_items.pop(self.bucket_name)
             self.list_items.update( {item_added:self.activity_list})
-        else:
-            raise ValueError
     def update_bucket(self,old_item , new_item):
         if new_item !=  "" and isinstance (new_item , str):
+            old_item_activity_list = self.list_items.get (old_item)
             self.list_items.pop(old_item)
-            self.list_items.update( {new_item:self.activity_list})
-    def delete_bucket(self):
-        self.list_items = { bucket_name : activity_list}
+            self.list_items.update( {new_item:old_item_activity_list})
+    def delete_bucket(self , deletable_bucket):
+        self.list_items.pop(deletable_bucket)
     def view_bucket(self):
         return self.bucket_name
-    def create_activity(self , item_added):
+    def create_activity(self , bucket_name , item_added):
         if item_added !=  "" and isinstance (item_added , str):
-             for item in self.activity_list:
-                if item.lower() == item_added.lower():
-                    raise ValueError
-                else:
-                    self.activity.list.append(item_added)
-    def delete_activity(self, activity_chosen):
-        self.activity_list.remove (activity_chosen)
-    def update_activity(self, old_activity , new_activity):
+            for key in self.list_items:
+                if key == bucket_name:
+                    temporary_list = self.list_items[key]
+                    temporary_list.append(item_added)
+                    self.list_items[key] = temporary_list
+    def delete_activity(self, bucket_name , activity_chosen):
+        self.list_items[bucket_name].remove (activity_chosen)
+    def update_activity(self, bucket_name , old_activity , new_activity):
         if new_activity !=  "" and isinstance (new_activity , str):
             counter = 0
-            while counter < len(self.activity_list):
-                if self.activity_list[counter] == old_activity:
-                    self.activity_list[counter] = new_activity
+            while counter < len(self.list_items[bucket_name]):
+                if self.list_items[bucket_name][counter] == old_activity:
+                    self.list_items[bucket_name][counter] = new_activity
                 counter +=1
     def read_activity_list():
         return self.activity_list
