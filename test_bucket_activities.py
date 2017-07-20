5a41c8e69fc78ea71fd6ce3cc8a9230c7c13f7e8
Winston <winstonkamau77@gmail.com>
2017-07-20 11:46:29 +0300

    Pylinted the test_user.py and test_bucket_activities.py

--- test_bucket_activities.py
+++ test_bucket_activities.py
@@ -4,39 +4,40 @@ import unittest
 from app.bucket_activities import BucketActivities
 
 class BucketActivitiesTestCase(unittest.TestCase):
+    '''A class that tests the crud for creating updating reading
+    and deleting bucket lists and activities'''
     def setUp(self):
         '''creating an object from the bucket_activities.py file'''
         self.bucket = BucketActivities()
     def test_create_bucket(self):
         '''Testing that the create bucket does create a bucket list'''
-        self.bucket.create_bucket ("Family")
-        self.assertEqual(self.bucket.list_items.__contains__("Family") , True) 
+        self.bucket.create_bucket("Family")
+        self.assertEqual(self.bucket.list_items.__contains__("Family"), True)
     def test_update_bucket(self):
         '''Testing that the update bucket does create a bucket list appropriately'''
-        self.bucket.list_items = { "Family": [ "visit" ]}
-        self.bucket.update_bucket( "Family" , "Travel" )
-        self.assertEqual(self.bucket.list_items , { "Travel": [ "visit" ]} ) 
+        self.bucket.list_items = {"Family": ["visit"]}
+        self.bucket.update_bucket("Family", "Travel")
+        self.assertEqual(self.bucket.list_items, {"Travel": ["visit"]})
     def test_delete_bucket(self):
         '''Testing that the delete bucket does delete a bucket list appropriately'''
-        self.bucket.list_items = { "Family": [ "visit" ] , "Travel" : [ "Algeria"] }
-        self.bucket.delete_bucket ( "Family" )
-        self.assertEqual(self.bucket.list_items , { "Travel": [ "Algeria" ]} )   
+        self.bucket.list_items = {"Family":["visit"], "Travel" : ["Algeria"]}
+        self.bucket.delete_bucket("Family")
+        self.assertEqual(self.bucket.list_items, {"Travel": ["Algeria"]})
     def test_create_activity(self):
         '''Testing that the create activity does create an activity list'''
-        self.bucket.list_items = { "Adventure" : ["hiking"]}
-        self.bucket.create_activity ( "Adventure" , "snowing")
-        self.assertEqual(self.bucket.list_items , { "Adventure" : ["hiking" , "snowing"]} ) 
+        self.bucket.list_items = {"Adventure" : ["hiking"]}
+        self.bucket.create_activity("Adventure", "snowing")
+        self.assertEqual(self.bucket.list_items, {"Adventure" : ["hiking", "snowing"]})
     def test_update_activity(self):
         '''Testing that the update activity does update an activity list appropriately'''
-        self.bucket.list_items = { "Family": [ "visit" ]}
-        self.bucket.update_activity ( "Family" , "visit" , "gettogether" )
-        self.assertEqual(self.bucket.list_items , { "Family": [ "gettogether" ]} )
+        self.bucket.list_items = {"Family": ["visit"]}
+        self.bucket.update_activity("Family", "visit", "gettogether")
+        self.assertEqual(self.bucket.list_items, {"Family":["gettogether"]})
     def test_delete_activity(self):
         '''Testing that the delete activity does delete an activity appropriately'''
-        self.bucket.list_items = { "Family": [ "visit" , "gettogether" ]  }
-        self.bucket.delete_activity ( "Family" , "visit" )
-        self.assertEqual(self.bucket.list_items , { "Family": [ "gettogether" ]} )   
-
+        self.bucket.list_items = {"Family": ["visit", "gettogether"]}
+        self.bucket.delete_activity("Family", "visit")
+        self.assertEqual(self.bucket.list_items, {"Family":["gettogether"]})
 
 if __name__ == '__main__':
     unittest.main()
--- test_user.py
+++ test_user.py
@@ -2,25 +2,26 @@
 import unittest
 #Importing the class u from the file user.py for testing
 from app.user import Users
-from flask import redirect , url_for
 
 class UsersTestCase(unittest.TestCase):
+    '''a class used to run tests on the Users class in user.py'''
     def setUp(self):
         '''creating an object from the user.py file'''
         self.user = Users()
     def test_login_entry(self):
-        result = self.user.login_user("user1" , "password1")
-        print ("The default users ",self.user.users)
-        print ("The user entered ",self.user.user_name)
-        print ("The password entered "+self.user.password)
-        self.assertEqual ( result , "user logged in" )
+        '''a method to test that the login in works'''
+        result = self.user.login_user("user1", "password1")
+        print("The default users ", self.user.users)
+        print("The user entered ", self.user.user_name)
+        print("The password entered "+self.user.password)
+        self.assertEqual(result, "user logged in")
     def test_log_out_user(self):
+        '''a method to test that the log out user function worked'''
         result = self.user.log_out_user()
-        self.assertEqual( result , "user logged out")
+        self.assertEqual(result, "user logged out")
     def test_create_user(self):
-        self.user.create_user( "user2", "password2", "password2")
-        self.assertEqual( self.user.users , { "user1": "password1" , "user2": "password2" })
-    
+        '''a method to test that a user was created successfully'''
+        self.user.create_user("user2", "password2", "password2")
+        self.assertEqual(self.user.users, {"user1": "password1", "user2": "password2"})
 if __name__ == '__main__':
     unittest.main()
-
