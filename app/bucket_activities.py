3f9573b8a045b68ec4bee545065ff173d90d5ef2
Winston <winstonkamau77@gmail.com>
2017-07-20 12:42:34 +0300

    pylinted bucket_activities.py to 10/10 and modified constant variables the init method

--- app/bucket_activities.py
+++ app/bucket_activities.py
@@ -1,14 +1,11 @@
 '''A module that contains a class that stores a bucket (key) and an activity list(value)'''
-bucket_name = ""
-activity_list = []
-list_items = {bucket_name : activity_list}
 class BucketActivities(object):
     ''' A class that reads , creates , updates and deletes bucket lists and activities'''
     def __init__(self):
         '''Creating instance variables for the class bucket activities'''
-        self.list_items = list_items
-        self.bucket_name = bucket_name
-        self.activity_list = activity_list
+        self.bucket_name = ""
+        self.activity_list = []
+        self.list_items = {self.bucket_name : self.activity_list}
     def create_bucket(self, item_added):
         '''A method that creates a bucket list'''
         if item_added != "" and isinstance(item_added, str):
@@ -40,7 +37,7 @@ class BucketActivities(object):
         '''A method that updates an activity'''
         if new_activity != "" and isinstance(new_activity, str):
             counter = 0
-            while counter < len(self.list_items[bucket_name]):
+            while counter < len(self.list_items[bucket_name_provided]):
                 if self.list_items[bucket_name_provided][counter] == old_activity:
                     self.list_items[bucket_name_provided][counter] = new_activity
                 counter += 1
