d2d2647135c86edc59c966c3d2ed04caf9558413
Winston <winstonkamau77@gmail.com>
2017-07-20 12:54:47 +0300

    made changes on the user.py

--- app/user.py
+++ app/user.py
@@ -4,11 +4,14 @@ from flask import redirect , url_for
 from app.bucket_activities import BucketActivities
 
 class Users():
+    '''A class for users of the app'''
     def __init__(self):
+        '''Initialising the variables for the Users class'''
         self.users = { "user1" : "password1"}
         self.bucket = BucketActivities
         self.user_buckets = {"user1":self.bucket}
     def login_user (self, user_name , password):
+        '''a method for login in a user and ensuring that the user name and password is not blank'''
         if (user_name != "" and password != "" ):
                 if isinstance ( user_name , str ):
                     self.user_name = user_name
@@ -21,13 +24,12 @@ class Users():
                         return "user not logged in"
         
     def log_out_user (self):
+        '''a method for logging out the user'''
         return "user logged out"
     def create_user(self, user_name, password, password_confirm):
+        '''a method for creating a user'''
             if user_name != "" and password != "" and isinstance ( user_name , str ) and password == password_confirm:
                 self.user_name = user_name
                 self.password = password
                 self.users.update({self.user_name:self.password})
                 self.user_buckets.update({self.user_name:self.bucket})
-
-
-
