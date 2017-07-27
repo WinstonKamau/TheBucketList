02fff1aa3f62c53203b27b3023b279054c15efdd
Winston <winstonkamau77@gmail.com>
2017-07-27 06:13:52 +0300

    deleted the file controller which was no longer important

--- app/controller.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class BucketListRegisterForm(FlaskForm):
-    '''form for handling registration'''
-    user-name = StringField ( 'username', validators = )
-   
