53f8bb53ad888e6d3b30d4f3334370b4cf5a441c
Winston <winstonkamau77@gmail.com>
2017-08-06 19:27:02 +0300

    Modified views.py to link to the model.py file

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@
 #importing the object render_template form flask for returning templates
 from flask import render_template, request, redirect, url_for
 from app import app
-from app.models import Users
-from app.models import BucketList
+from app.model import Users
+from app.model import BucketList
 
 #binding register function to the url /Register
 @app.route('/Register', methods = ['GET', 'POST'])
@@ -55,7 +55,7 @@ def edit_bucket_list(index=None):
             return redirect(url_for('view_bucket_list'))
         else:
             error = "You cannot replace the bucket name with an empty string!"
-    bucket_items = BucketList().bucket_list
+    bucket_items = BucketList().view_bucket()
     index_integer = int(index)
     return render_template("EditBucketList.html", error=error, bucket_items=bucket_items, index_integer = index_integer)
 @app.route('/DeleteBucketList/<index>', methods = ['GET', 'POST'])
@@ -63,7 +63,7 @@ def delete_bucket_list(index=None):
     if request.method == "POST":
         BucketList().delete_bucket(int(index))
         return redirect(url_for('view_bucket_list'))
-    bucket_item_chosen = BucketList().bucket_list[int(index)].bucket_name
+    bucket_item_chosen = BucketList().view_bucket()[int(index)].bucket_name
     return render_template("DeleteBucketList.html", bucket_item_chosen=bucket_item_chosen )
 #binding view_bucket_list function to the url /ViewBucketList
 @app.route('/ViewBucketList', methods = ['GET', 'POST' ])
@@ -73,13 +73,13 @@ def view_bucket_list():
     if request.method == "POST":
         return redirect (url_for('create_bucket_list'))
     else:
-        bucket_items = BucketList().bucket_list
+        bucket_items = BucketList().view_bucket()
         return render_template("ViewBucketList.html", bucket_items=bucket_items, prompt=prompt)
 #binding view_activities function to the url /ViewActivities
 @app.route('/ViewActivities/<index>')
 def view_activities(index):
     '''a method that returns the route of viewactivities in the html'''
-    activity_items = BucketList().bucket_list[int(index)].activity_list
+    activity_items = BucketList().view_activity(int(index))
     spacer = "x"
     prompt = "You currently have no activities on the activity list, press the create activity button to create a new activity!"
     return render_template("ViewActivities.html", activity_items=activity_items, index=index, spacer=spacer, prompt=prompt)
@@ -108,7 +108,7 @@ def edit_activities(indices):
             return redirect('/ViewActivities/'+bucket_index)
         else:
             error ="You cannot replace activity name with an empty string"
-    the_activity= BucketList().bucket_list[int(bucket_index)].activity_list[int(activity_index)]
+    the_activity= BucketList().view_bucket()[int(bucket_index)].activity_list[int(activity_index)]
     return render_template("EditActivity.html", error=error, the_activity=the_activity )
 @app.route('/DeleteActivity/<indices>', methods = ['GET','POST'])
 def delete_activities(indices):
@@ -118,12 +118,10 @@ def delete_activities(indices):
     if request.method == "POST":
         BucketList().delete_activity(int(bucket_index), int(activity_index))
         return redirect('/ViewActivities/'+bucket_index)
-    activity_item_chosen = BucketList().bucket_list[int(bucket_index)].activity_list[int(activity_index)]
+    activity_item_chosen = BucketList().view_bucket()[int(bucket_index)].activity_list[int(activity_index)]
     return render_template("DeleteActivity.html", activity_item_chosen=activity_item_chosen )
 @app.route('/Logout')
 def log_out():
     '''a method that logs out the user'''
-    Users().users.clear()
-    BucketList().bucket_list.clear()
     return redirect('/')
 
