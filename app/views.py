314024c22aec92cddc6e69c51dd48b8956414613
Winston <winstonkamau77@gmail.com>
2017-08-07 14:10:47 +0300

    Pylinting views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-'''A module used to listen on the root of the application and will select
- title and text from the database '''
+'''A module used to return views.  '''
 #importing the object render_template form flask for returning templates
 from flask import render_template, request, redirect, url_for
 from app import app
@@ -7,41 +6,43 @@ from app.models import Users
 from app.models import BucketList
 
 #binding register function to the url /Register
-@app.route('/Register', methods = ['GET', 'POST'])
+@app.route('/Register', methods=['GET', 'POST'])
 def register():
-    '''a method that returns the route of register in the html'''  
+    '''A method that returns the route of register in the html.  '''
     error = ""
     if request.method == "POST":
         user_name = request.form['username_register']
         user_email = request.form['useremail_register']
         password = request.form['password_register']
         new_password = request.form['confirm_new_password']
-        if Users().create_user(user_name, user_email, password , new_password) == "all values okay":
+        if Users().create_user(user_name, user_email, password, new_password) == "all values okay":
             return redirect(url_for('login'))
-        elif Users().create_user(user_name, user_email, password , new_password) == "similar email":
+        elif Users().create_user(user_name, user_email, password, new_password) == "similar email":
             error = user_email+" is already registered"
-        elif Users().create_user(user_name, user_email, password , new_password) == "similar name":
+        elif Users().create_user(user_name, user_email, password, new_password) == "similar name":
             error = user_name+" is already registered"
         else:
             error = "Ensure that all fields have been inserted and that the passwords are the same!"
     return render_template("Register.html", error=error)
+
 #binding create_bucket_list function to the url /CreateBucketList
 #binding login function to the url /
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    '''a method that returns the route of login in the html'''
+    '''A method that returns the route of login in the html.  '''
     error = ""
-    if request.method =="POST":
+    if request.method == "POST":
         user_email = request.form['user_email_login']
         user_password = request.form['password_login']
         if Users().login_user(user_email, user_password):
             return redirect(url_for('view_bucket_list'))
         else:
             error = "Invalid email / password!"
-    return render_template("Login.html" , error=error)
-@app.route('/CreateBucketList', methods = ['GET', 'POST'])
+    return render_template("Login.html", error=error)
+
+@app.route('/CreateBucketList', methods=['GET', 'POST'])
 def create_bucket_list():
-    '''a method that returns the route of createbucketlist in the html'''
+    '''A method that returns the route of createbucketlist in the html.  '''
     error = ""
     if request.method == "POST":
         bucket_name = request.form['bucketlistcreated']
@@ -51,52 +52,63 @@ def create_bucket_list():
             if bucket_name == "":
                 error = "Ensure that the bucket name is not an empty word!"
             else:
-                error = "'"+bucket_name+"' already exists in the bucket list!" 
+                error = "'"+bucket_name+"' already exists in the bucket list!"
     return render_template("CreateBucketList.html", error=error)
-@app.route('/EditBucketList/<index>', methods = ['GET', 'POST'])
+
+@app.route('/EditBucketList/<index>', methods=['GET', 'POST'])
 def edit_bucket_list(index=None):
+    '''A method that returns the route of the views if right input is made.  '''
     error = ""
     if request.method == "POST":
         new_name = request.form['new_name']
-        if BucketList().edit_bucket( int(index), new_name):
+        if BucketList().edit_bucket(int(index), new_name):
             return redirect(url_for('view_bucket_list'))
         else:
             if new_name == "":
                 error = "Bucket cannot be replaced with an empty string!"
             else:
-                error ="'"+new_name+"' already exists in the bucket list!"
+                error = "'"+new_name+"' already exists in the bucket list!"
     bucket_items = BucketList().view_bucket()
     index_integer = int(index)
-    return render_template("EditBucketList.html", error=error, bucket_items=bucket_items, index_integer = index_integer)
-@app.route('/DeleteBucketList/<index>', methods = ['GET', 'POST'])
+    return render_template("EditBucketList.html", error=error,
+                           bucket_items=bucket_items, index_integer=index_integer)
+
+@app.route('/DeleteBucketList/<index>', methods=['GET', 'POST'])
 def delete_bucket_list(index=None):
+    '''A method that returns the view for viewbucketlist after the user has opted to delete
+    or to not delete.  '''
     if request.method == "POST":
         BucketList().delete_bucket(int(index))
         return redirect(url_for('view_bucket_list'))
     bucket_item_chosen = BucketList().view_bucket()[int(index)].bucket_name
-    return render_template("DeleteBucketList.html", bucket_item_chosen=bucket_item_chosen )
+    return render_template("DeleteBucketList.html", bucket_item_chosen=bucket_item_chosen)
+
 #binding view_bucket_list function to the url /ViewBucketList
-@app.route('/ViewBucketList', methods = ['GET', 'POST' ])
+@app.route('/ViewBucketList', methods=['GET', 'POST'])
 def view_bucket_list():
-    '''a method that returns the route of viewbucketlist in the html'''
-    prompt = "You currently have no bucketlists on the bucket list, press the create new bucket list button to create a new bucket!"
+    '''A method that returns the route of viewbucketlist in the html.  '''
+    prompt = "You currently have no bucketlists on the bucket list,"
+    prompt += " press the create new bucket list button to create a new bucket!"
     if request.method == "POST":
-        return redirect (url_for('create_bucket_list'))
-    else:
-        bucket_items = BucketList().view_bucket()
-        return render_template("ViewBucketList.html", bucket_items=bucket_items, prompt=prompt)
+        return redirect(url_for('create_bucket_list'))
+    bucket_items = BucketList().view_bucket()
+    return render_template("ViewBucketList.html", bucket_items=bucket_items, prompt=prompt)
+
 #binding view_activities function to the url /ViewActivities
 @app.route('/ViewActivities/<index>')
 def view_activities(index):
-    '''a method that returns the route of viewactivities in the html'''
+    '''A method that returns the route of viewactivities in the html.  '''
     activity_items = BucketList().view_activity(int(index))
     spacer = "x"
-    prompt = "You currently have no activities on the activity list, press the create activity button to create a new activity!"
-    return render_template("ViewActivities.html", activity_items=activity_items, index=index, spacer=spacer, prompt=prompt)
+    prompt = "You currently have no activities on the activity list, "
+    prompt += "press the create activity button to create a new activity!"
+    return render_template("ViewActivities.html", activity_items=activity_items,
+                           index=index, spacer=spacer, prompt=prompt)
+
 #binding add_Activities function to the url /AddActivities
-@app.route('/AddActivities/<bucket_index>', methods = ['GET', 'POST'])
+@app.route('/AddActivities/<bucket_index>', methods=['GET', 'POST'])
 def add_activities(bucket_index):
-    '''a method that returns the route of addactivities in the html'''
+    '''A method that returns the route of addactivities in the html.  '''
     error = ""
     if request.method == "POST":
         activity = request.form['theActivity']
@@ -108,10 +120,11 @@ def add_activities(bucket_index):
             else:
                 error = "'"+activity+"' already exists in the activity list!"
     return render_template("AddActivities.html", error=error)
+
 #binding add_Activities function to the url /AddActivities
-@app.route('/EditActivity/<indices>', methods = ['GET', 'POST'])
+@app.route('/EditActivity/<indices>', methods=['GET', 'POST'])
 def edit_activities(indices):
-    '''a method that returns the route of addactivities in the html'''
+    '''A method that returns the route of addactivities in the html.  '''
     error = ""
     bucket_index = indices.split("x")[0]
     activity_index = indices.split("x")[1]
@@ -124,20 +137,21 @@ def edit_activities(indices):
                 error = "An activity cannot be replaced with an empty string"
             else:
                 error = "'"+new_activity+"' already exists in the activity list"
-    the_activity= BucketList().view_bucket()[int(bucket_index)].activity_list[int(activity_index)]
-    return render_template("EditActivity.html", error=error, the_activity=the_activity )
-@app.route('/DeleteActivity/<indices>', methods = ['GET','POST'])
+    the_activity = BucketList().view_bucket()[int(bucket_index)].activity_list[int(activity_index)]
+    return render_template("EditActivity.html", error=error, the_activity=the_activity)
+
+@app.route('/DeleteActivity/<indices>', methods=['GET', 'POST'])
 def delete_activities(indices):
-    '''a method that deletes an activity'''
+    '''A method that deletes an activity.  '''
     bucket_index = indices.split("x")[0]
     activity_index = indices.split("x")[1]
     if request.method == "POST":
         BucketList().delete_activity(int(bucket_index), int(activity_index))
         return redirect('/ViewActivities/'+bucket_index)
     activity_item_chosen = BucketList().view_bucket()[int(bucket_index)].activity_list[int(activity_index)]
-    return render_template("DeleteActivity.html", activity_item_chosen=activity_item_chosen )
+    return render_template("DeleteActivity.html", activity_item_chosen=activity_item_chosen)
+
 @app.route('/Logout')
 def log_out():
-    '''a method that logs out the user'''
+    '''A method that logs out the user.  '''
     return redirect('/')
-
