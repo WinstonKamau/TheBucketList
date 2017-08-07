3cfd2713df00739fa30bc4fd5be2eac7c45e40ef
Winston <winstonkamau77@gmail.com>
2017-08-07 14:13:09 +0300

    Pylinting teste_login.py to 10/10

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,35 +1,40 @@
-'''A test file for testing the login method'''
+'''A test file for testing the login method.  '''
 import unittest
-#Importing the modules.py file to access its global variables
 from app import models
-#Importing the class Users from the module models.py
 from app.models import Users
 
+
 class LoginTestCase(unittest.TestCase):
-    '''a class used to run tests on the Users class in models.py'''
+    '''A class used to run tests on the Users class in models.py.  '''
+
+
     def setUp(self):
-        '''creating an object from the models.py file'''
+        '''Creating an object from the models.py file for the class Users.  '''
         self.user = Users()
+
     def test_login_entry_happy_path(self):
-        '''a method to test that the login in works happy path'''
+        '''A method to test that the login in works with the right inputs.  '''
         models.users.clear()
         self.user.create_user("user1", "user1@gmail.com", "password1", "password1")
         boolean_for_login_user = self.user.login_user("user1@gmail.com", "password1")
         self.assertEqual(boolean_for_login_user, True)
+
     def test_login_entry_sad_path_1(self):
-        '''a method to test that login does not work with wrong input of user email'''
+        '''A method to test that login does not work with wrong input of user email.  '''
         models.users.clear()
         self.user.create_user("user1", "user1@gmail.com", "password1", "password1")
         boolean_for_login_user = self.user.login_user("user2@gmail.com", "password1")
         self.assertEqual(boolean_for_login_user, None)
+
     def test_login_entry_sad_path_2(self):
-        '''a method to test that login does not work with wrong input of password'''
+        '''A method to test that login does not work with wrong input of password.  '''
         models.users.clear()
         self.user.create_user("user1", "user1@gmail.com", "password1", "password1")
         boolean_for_login_user = self.user.login_user("user1@gmail.com", "password2")
         self.assertEqual(boolean_for_login_user, None)
+
     def test_login_sets_right_user_id(self):
-        '''a method to test that the right user id is set on login'''
+        '''A method to test that the right user id is set on login.  '''
         models.users.clear()
         self.user.create_user("user1", "user1@gmail.com", "password1", "password1")
         self.user.create_user("user2", "user2@gmail.com", "password2", "password2")
@@ -40,8 +45,6 @@ class LoginTestCase(unittest.TestCase):
         self.assertEqual(models.user_id, 1)
         self.user.login_user("user1@gmail.com", "password1")
         self.assertEqual(models.user_id, 0)
-    
 
-    
 if __name__ == '__main__':
     unittest.main()
