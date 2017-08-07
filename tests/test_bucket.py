f26dcb68f9a06f40b3bac6f2b688706adfde1dde
Winston <winstonkamau77@gmail.com>
2017-08-07 11:40:27 +0300

    Refining the tests to the changes of adding global variables to the models.py file and testing that a bucket creation or replacement does not do so for an bucket name input that is already on the bucket list

--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -1,5 +1,9 @@
 '''A test file for testing the BucketList class in models.py file'''
 import unittest
+#Importing the module models.py to access its global variable users
+from app import models
+#Importing the class Users from the file models.py
+from app.models import Users
 #Importing the class BucketList from the file models.py for testing
 from app.models import BucketList
 
@@ -8,31 +12,64 @@ class BucketActivitiesTestCase(unittest.TestCase):
     and deleting bucket lists '''
     def test_create_bucket_happy_path(self):
         '''Testing that the create bucket does create a bucket list object'''
-        BucketList.bucket_list.clear()
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
         BucketList().create_bucket("Family")
-        self.assertEqual(len(BucketList().bucket_list), 1)
+        self.assertEqual(len(models.users[0].user_bucket), 1)
     def test_create_bucket_happy_path_1(self):
         '''Testing that the create bucket does create a bucket list object with the right bucket name'''
-        self.assertEqual(BucketList().bucket_list[0].bucket_name, "Family")
-    def test_create_bucket_sad_path_(self):
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
+        BucketList().create_bucket("Family")
+        self.assertEqual(models.users[0].user_bucket[0].bucket_name, "Family")
+    def test_create_bucket_sad_path(self):
         '''Testing that the create bucket does not create a bucket list object with an empty bucket name'''
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
+        BucketList().create_bucket("Family")
         BucketList().create_bucket("")
-        self.assertEqual(len(BucketList().bucket_list), 1)
+        self.assertEqual(len(models.users[0].user_bucket), 1)
+    def test_create_bucket_sad_path_1(self):
+        '''Testing that the create bucket does not create a bucket list object
+        that is similar in name to a previous bucket object name created'''
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
+        BucketList().create_bucket("Family")
+        BucketList().create_bucket("Family")
+        self.assertEqual(len(models.users[0].user_bucket), 1)
     def test_edit_bucket_happy_path(self):
-        '''Testing that the edit bucket does not edit a particulart bucket list object'''
+        '''Testing that the edit bucket edit a particular bucket list object with 
+        right arguments entered'''
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
         BucketList().create_bucket("Family")
         BucketList().edit_bucket(0, "Travel")
-        self.assertEqual(BucketList().bucket_list[0].bucket_name, "Travel")
+        self.assertEqual(models.users[0].user_bucket[0].bucket_name, "Travel")
     def test_edit_bucket_sad_path(self):
-        '''testing that the edit bucket does not edit once an empty string is given'''
+        '''testing that the edit bucket does not edit once an empty string is given
+        or when a bucket name entered already exists in the user's bucket list'''
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
+        BucketList().create_bucket("Family")
         BucketList().edit_bucket(0, "")
-        self.assertEqual(BucketList().bucket_list[0].bucket_name, "Travel")
+        self.assertEqual(models.users[0].user_bucket[0].bucket_name, "Family")
+        BucketList().create_bucket("Travel")
+        BucketList().edit_bucket(0, "Travel")
+        self.assertEqual(models.users[0].user_bucket[0].bucket_name, "Family")
     def test_delete_bucket_happy_path(self):
         '''Testing that the delete bucket does work'''
-        BucketList.bucket_list.clear()
-        BucketList().create_bucket("Adventure")
+        models.users.clear()
+        Users().create_user("user1", "user1@gmail.com", "password", "password")
+        Users().login_user("user1@gmail.com", "password")
+        BucketList().create_bucket("Family")
         BucketList().delete_bucket(0)
-        self.assertEqual(len(BucketList().bucket_list), 0)
+        self.assertEqual(len(models.users[0].user_bucket), 0)
 
 if __name__ == '__main__':
     unittest.main()
